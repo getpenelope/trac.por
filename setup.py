052186110328bf52177189026df23f01e9d7e52d
Andrew Mleczko <andrew@mleczko.net>
2013-01-30 12:40:43 +0100

    add manifest

--- setup.py
+++ setup.py
@@ -3,7 +3,10 @@ import os
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.md')).read()
+README = """trac.por
+=============
+
+for more details visit: http://getpenelope.github.com/"""
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
