17cab0030a50a2d5956f4887b38ce2234256eef1
Andrew Mleczko <andrew@mleczko.net>
2013-04-22 08:56:49 +0200

    Preparing release 1.2.12

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ except ImportError:
 
 
 setup(name='trac.por',
-      version='1.2.12.dev0',
+      version='1.2.12',
       description='Penelope: trac integration',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
