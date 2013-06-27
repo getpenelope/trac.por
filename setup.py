13a55c259de1436822ce5aa716ff09d83aa216fc
Andrew Mleczko <andrew@mleczko.net>
2013-06-27 09:57:53 +0200

    Back to development: 1.2.17

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ except ImportError:
 
 
 setup(name='trac.por',
-      version='1.2.16',
+      version='1.2.17.dev0',
       description='Penelope: trac integration',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
